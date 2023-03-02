c11ddae9980d551473a0fc3dc65832de14a2e14e
Vladislav Voitov <dobrihlopez@gmail.com>
2023-03-02 21:15:43 +0300

    add snackbar

--- app/src/main/java/com/voitov/vknewsclient/ui/theme/NewsMainScreen.kt
+++ app/src/main/java/com/voitov/vknewsclient/ui/theme/NewsMainScreen.kt
@@ -4,56 +4,93 @@ import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 const val TAG = "COMPOSE_TEST"
 
 @Preview
 @Composable
 fun VkNews() {
-    Scaffold(
-        bottomBar = {
-            BottomNavigation {
-                Log.d(TAG, "recomposed")
-                val items =
-                    listOf(NavigationItem.Profile, NavigationItem.Home, NavigationItem.Favorite)
-                val startFocusedSection = 0
-                val selectedItemIndex = remember {
-                    mutableStateOf(startFocusedSection)
-                }
+    VkNewsClientTheme {
+        val snackBarHostState = remember {
+            SnackbarHostState()
+        }
+        var fabIsVisible by remember {
+            mutableStateOf(true)
+        }
+        val scope = rememberCoroutineScope()
 
-                items.forEachIndexed { index, navigationItem ->
-                    BottomNavigationItem(
-                        onClick = {
-                            selectedItemIndex.value = index
-                        },
-                        icon = {
-                            Icon(
-                                navigationItem.icon,
-                                contentDescription = stringResource(navigationItem.labelResId)
+        Log.d(TAG, "vkNews")
+
+        Scaffold(
+            bottomBar = {
+                Log.d(TAG, "bottomBar")
+                BottomNavigation {
+                    Log.d(TAG, "bottom navigation")
+                    val items =
+                        listOf(NavigationItem.Profile, NavigationItem.Home, NavigationItem.Favorite)
+                    val startFocusedSection = 0
+                    val selectedItemIndex = remember {
+                        mutableStateOf(startFocusedSection)
+                    }
+
+                    items.forEachIndexed { index, navigationItem ->
+                        BottomNavigationItem(
+                            onClick = {
+                                selectedItemIndex.value = index
+                            },
+                            icon = {
+                                Icon(
+                                    navigationItem.icon,
+                                    contentDescription = stringResource(navigationItem.labelResId)
+                                )
+                            },
+                            label = {
+                                Text(stringResource(navigationItem.labelResId))
+                            },
+                            selected = selectedItemIndex.value == index,
+                            selectedContentColor = MaterialTheme.colors.onPrimary,
+                            unselectedContentColor = MaterialTheme.colors.onSecondary,
+                        )
+                    }
+                }
+            },
+            floatingActionButton = {
+                Log.d(TAG, "FAB")
+                if (fabIsVisible) {
+                    FloatingActionButton(onClick = {
+                        scope.launch {
+                            val snackBarResult = snackBarHostState.showSnackbar(
+                                "This is a snackbar",
+                                "Okay",
+                                SnackbarDuration.Long,
                             )
-                        },
-                        label = {
-                            Text(stringResource(navigationItem.labelResId))
-                        },
-                        selected = selectedItemIndex.value == index,
-                        selectedContentColor = MaterialTheme.colors.onPrimary,
-                        unselectedContentColor = MaterialTheme.colors.onSecondary,
-                    )
-                }
-            }
-        },
 
+                            if (snackBarResult == SnackbarResult.ActionPerformed) {
+                                fabIsVisible = false
+                            }
+                        }
+                    }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = "")
+                    }
+                }
+            },
+            snackbarHost = {
+                Log.d(TAG, "Snackbar")
+                SnackbarHost(hostState = snackBarHostState)
+            }
         ) {
-        Column(modifier = Modifier.padding(all = 8.dp)) {
-            repeat(1) {
-                NewsPost()
+            Column(modifier = Modifier.padding(all = 8.dp)) {
+                repeat(1) {
+                    NewsPost()
+                }
             }
         }
     }
